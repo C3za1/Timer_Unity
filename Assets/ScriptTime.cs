d663551fce24f82d0aae3d0bf62be3062491fb80
C3za1 <C3za1.97@gmail.com>
2020-04-27 12:11:35 -0500

    Time_Unity
    Muestra un cronometro del tiempo que le asignes

---
+++ Assets/ScriptTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScriptTime : MonoBehaviour
+{
+    public Text Contador;
+    public Text fin;
+    private float tiempo = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Contador.text = "" + tiempo;
+        fin.enabled = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo -= Time.deltaTime;
+        Contador.text = " " + tiempo.ToString("f0") ;
+
+        if (tiempo <= 0)
+        {
+            Contador.text = "0";
+            fin.enabled = true;
+        }
+    }
+}
